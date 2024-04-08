dd1b9eb594a82648c2e9f3f908a42b605de0a0c4
syex0x <s.r.mahamid@gmail.com>
2024-04-08 17:20:36 +0500

    Practice

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 class CustomBoxLayout(BoxLayout):
-    def __init__(self, **kwargs):
+  """  def __init__(self, **kwargs):
         super().__init__(**kwargs)
         b1 = Button(text="1")
         b2 = Button(text="2")
-
+        
         self.add_widget(b1)
-        self.add_widget(b2)
+        self.add_widget(b2)"""
 
 class MainWidget(Widget):
     pass
