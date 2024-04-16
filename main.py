db72c90271eec33013edf324be34819eb9315820
syex0x <s.r.mahamid@gmail.com>
2024-04-16 11:51:19 +0500

    Done

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 
+class CustomStackLayout(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        x=0.05
+        for i in range(1,11):
+            b=Button(text=str(i), size_hint=(x,x))
+            self.add_widget(b)
+            x=x+0.05
+
 class CustomAnchorLayout(AnchorLayout):
+    pass
+
+class CustomGridLayout(GridLayout):
     pass
 
 class CustomBoxLayout(BoxLayout):
