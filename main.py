a5dd520a77221232c6172bcf5a66b3c7e731da65
syex0x <s.r.mahamid@gmail.com>
2024-04-17 22:33:34 +0500

    Why is this not working?!!!

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.anchorlayout import AnchorLayout
+
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 
+class CustomScrollView(ScrollView):
+    pass
+
 class CustomStackLayout(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        x=0.05
-        for i in range(1,11):
-            b=Button(text=str(i), size_hint=(x,x))
+        self.orientation="lr-tb"
+     
+        for i in range(1,10):
+            b=Button(text=str(i), size_hint=(.2,.2))
             self.add_widget(b)
-            x=x+0.05
+            
 
 class CustomAnchorLayout(AnchorLayout):
     pass
